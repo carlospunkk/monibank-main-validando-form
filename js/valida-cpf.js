8cf514cf6f3b434f48e7c5e32d69947711b0c27c
carlos romulo vilela silva <cromulo.romulo@gmail.com>
2023-10-10 21:07:54 -0300

    validaprimeirodigito

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,6 +1,8 @@
 export default function ehumCpf(campo){
  const cpf = campo.value.replace(/\.|-/g, ""); // replace recebe dois parametros o que a gente quer substituir e o segundo pelo que queremos substituir
  validaNumerosRepetidos(cpf)
+ validaprimeiroDigito(cpf)
+ 
  console.log(validaNumerosRepetidos(cpf))
 }
 
@@ -19,4 +21,21 @@ function validaNumerosRepetidos(cpf){
   ]
 
   return numerosRepetidos.includes(cpf) // percorre a lista e retorna verdadeiro se achar os numeros repetidos
+}
+
+function validaprimeiroDigito(cpf){
+  let soma = 0 ;
+  let multiplicador =10;
+
+  for(tamanho = 0 ; tamanho <=9 ; tamanho++){
+    soma += cpf[tamanho] * multiplicador;
+    multiplicador-- 
+  }
+  soma = (soma * 10) % 11;
+
+  if(soma == 10 || soma == 11){
+    soma = 0;
+
+  }
+  return soma != cpf[9];
 }
